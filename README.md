a88cd6f474d4ec38db373b87a0aba32882a4b697
Kim <lademokim@hotmail.com>
2023-02-07 20:01:19 +0100

    Update REEADME

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ Run in IDE and send a HTTP request to the server on port 8080 by either using yo
 # Observation
 We can see a significant increase in efficeny when using multithreaded server than the singlethreaded server.
 When connecting with multiple clients we do not have to wait for a task to finish on the multithreaded server before starting on the next task, meanwhile the singlethreaded have to wait for each one to finish.
+
+[singlethreaded](documentation/singlethreaded.png)
+[multithreaded](documentation/multithreaded.png)
--- src/main/java/no/ntnu/Main.java
+++ src/main/java/no/ntnu/Main.java
@@ -1,17 +1,18 @@
 package no.ntnu;
 
 import no.ntnu.servers.MultiThreadedServer;
+import no.ntnu.servers.SingleThreadedServer;
 
 class Main {
     public static void main(String[] args) throws Exception {
-//        System.out.println("Starting SingleThread server");
-//        SingleThreadedServer server = new SingleThreadedServer(8080);
-//        Thread serverThread = new Thread(server);
-//        serverThread.start();
-
-        System.out.println("Starting MultiThread server");
-        MultiThreadedServer server = new MultiThreadedServer(8080);
+        System.out.println("Starting SingleThread server");
+        SingleThreadedServer server = new SingleThreadedServer(8080);
         Thread serverThread = new Thread(server);
         serverThread.start();
+
+//        System.out.println("Starting MultiThread server");
+//        MultiThreadedServer server = new MultiThreadedServer(8080);
+//        Thread serverThread = new Thread(server);
+//        serverThread.start();
     }
 }
